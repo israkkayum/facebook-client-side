3c60b2707466330c63d95ee45b1560cf82b07351
Israk Kayum <israkkayumchowdhury@israks-MacBook-Air.local>
2022-01-27 12:35:43 +0600

    third

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -50,7 +50,7 @@ const Body = () => {
             ...post
         }
 
-        fetch('https://facebook-server-side.herokuapp.com/posts', {
+        fetch('http://facebook-server-side.herokuapp.com/posts', {
             method: 'POST',
             headers: {
                 'content-type': 'application/json'
